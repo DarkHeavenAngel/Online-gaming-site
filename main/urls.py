546362b659bc8327c98cffadbdb6f1320a3f049b
DarkHeavenAngel <daria.melnyk.ukr@gmail.com>
2025-10-07 21:37:37 +0300

    Fixed a problem in urls

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from main.views import GamesList, GameDetail, AuthorsList, AuthorDetail, PublisherDetail
+from main.views import GamesList, GameDetail, AuthorsList, AuthorDetail, PublisherDetail, PublishersList
 
 urlpatterns = [
     path('games/', GamesList.as_view()),
     path('games/<int:pk>/', GameDetail.as_view()),
     path('authors/', AuthorsList.as_view()),
     path('authors/<int:pk>/', AuthorDetail.as_view()),
-    path('publishers/', PublisherDetail.as_view()),
+    path('publishers/', PublishersList.as_view()),
     path('publishers/<int:pk>/', PublisherDetail.as_view()),
 ]
 
